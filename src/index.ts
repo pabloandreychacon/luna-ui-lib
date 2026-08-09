c2e1c614a7ccd12081bd0df1093f0c1d61db8226
pabloandreychacon <pabloandreychacon@hotmail.com>
2026-08-08 19:53:19 -0600

    pub

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './components/ui-datatable.js';
-export * from './components/ui-fab.js';
-export * from './components/ui-multiselect.js';
-export * from './components/ui-popconfirm.js';
-export * from './components/ui-spinner.js';
-export * from './components/ui-toast.js';
